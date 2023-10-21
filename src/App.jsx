28e219acba499cbd318e078649924d7b1d831678
Ivan Falcon <Falconivanernesto@gmail.com>
2023-10-21 00:26:26 -0300

    Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Home from "./assets/components/Home";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
     return (
         <>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
